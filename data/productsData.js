9ff375e15473f3c33393eff2d9eab6abddbbbd91
Witor Tenã <witortena@gmail.com>
2026-04-13 16:07:56 -0300

    Add new products to productsData.js

--- data/productsData.js
+++ data/productsData.js
@@ -1,23 +1,63 @@
 let products = [
-  {
-    name: "Banana",
-    price: 200,
-    id: 1,
-  },
-  {
-    name: "Chinelo",
-    price: 2,
-    id: 2,
-  },
-  {
-    name: "Cachorro dos Himalaias",
-    price: 120,
-    id: 3,
-  },
-  {
-    name: "Ônibus Federal do Maranhão",
-    price: 15460,
-    id: 4,
-  },
+ {
+  name: "Banana",
+  price: 200,
+  id: 1,
+},
+{
+  name: "Chinelo",
+  price: 2,
+  id: 2,
+},
+{
+  name: "Cachorro dos Himalaias",
+  price: 120,
+  id: 3,
+},
+{
+  name: "Ônibus Federal do Maranhão",
+  price: 15460,
+  id: 4,
+},
+{
+  name: "Teclado Mecânico RGB",
+  price: 350,
+  id: 5,
+},
+{
+  name: "Mouse Gamer 16000 DPI",
+  price: 180,
+  id: 6,
+},
+{
+  name: "Cadeira Gamer Ultra Conforto",
+  price: 1200,
+  id: 7,
+},
+{
+  name: "Monitor 144Hz",
+  price: 950,
+  id: 8,
+},
+{
+  name: "Café Premium 1kg",
+  price: 45,
+  id: 9,
+},
+{
+  name: "Fone de Ouvido Bluetooth",
+  price: 220,
+  id: 10,
+},
+{
+  name: "Notebook ThinkPad T14",
+  price: 4500,
+  id: 11,
+},
+{
+  name: "Curso Fullstack Dev",
+  price: 300,
+  id: 12,
+}
 ];
 module.exports = products;
